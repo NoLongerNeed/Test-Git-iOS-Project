4258034e67ebfb52f35f5b30087502188b62e590
David Kearhs <David@Davids-Mac.local>
2014-08-12 14:17:14 +1000

    Initial Commit

---
+++ Git Test/DJHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DJHAppDelegate.h
+//  Git Test
+//
+//  Created by David Kearhs on 12/08/2014.
+//  Copyright (c) 2014 David Kearhs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DJHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/DJHViewController.h
@@ -0,0 +1,13 @@
+//
+//  DJHViewController.h
+//  Git Test
+//
+//  Created by David Kearhs on 12/08/2014.
+//  Copyright (c) 2014 David Kearhs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DJHViewController : UIViewController
+
+@end
